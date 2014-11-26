80c06eaf6472f3f863e9c244e56b16f647851947
Kevin L. Esteb <kevin@kesteb.us>
2014-11-26 13:34:17 -0800

    Added a way to generate sql to create a database

---
+++ trunk/lib/XAS/Apps/Database/Schema.pm
@@ -0,0 +1,124 @@
+package XAS::Apps::Database::Schema;
+
+use XAS::Model::Database;
+
+use XAS::Class
+  debug     => 0,
+  version   => '0.02',
+  base      => 'XAS::Lib::App',
+  accessors => 'dbtype revision directory schema',
+;
+
+# ----------------------------------------------------------------------
+# Public Methods
+# ----------------------------------------------------------------------
+
+sub setup {
+    my $self = shift;
+
+    $self->{schema} = XAS::Model::Database->opendb('database');
+
+}
+
+sub main {
+    my $self = shift;
+
+    $self->setup();
+
+    $self->log->info('starting up');
+
+    $self->schema->create_ddl_dir(
+        [$self->dbtype],
+        $self->revision,
+        $self->directory,
+    );
+
+    $self->log->info('shutting down');
+
+}
+
+sub options {
+    my $self = shift;
+
+    $self->{dbtype}    = 'SQLite';
+    $self->{revision}  = '0.01';
+    $self->{directory} = './sql/';
+
+    return {
+        'dbtype=s'    => \$self->{dbtype},
+        'revision=s'  => \$self->{revision},
+        'directory=s' => \$self->{directory},
+    };
+
+}
+
+# ----------------------------------------------------------------------
+# Private Methods
+# ----------------------------------------------------------------------
+
+1;
+
+__END__
+
+=head1 NAME
+
+XAS::Apps::Database::Schema - Create a database schema
+
+=head1 SYNOPSIS
+
+ use XAS::Apps::Database::Schema;
+
+ my $app = XAS::Apps::Database::Schema->new();
+
+ exit $app->run();
+
+=head1 DESCRIPTION
+
+This module will create a schema for the XAS database. It inherits from
+L<XAS::Lib::App>. Please see that module for additional documentation.
+
+=head1 OPTIONS
+
+This modules provides these additonal cli options.
+
+=head2 --dbtype
+
+The type of database. This can be one of the following:
+
+ PostgreSQL 
+ SQLite
+ MySQL
+
+Or any other SQL::Translater database name.
+
+=head2 --revision
+
+The revision for this schema. Defaults to "0.01".
+
+=head2 --directory
+
+The directory to write the schema into. Defaults to "./sql.".
+
+=head1 SEE ALSO
+
+=over 4
+
+=item bin/xas-create-schema
+
+=item L<XAS|XAS>
+
+=back
+
+=head1 AUTHOR
+
+Kevin L. Esteb, E<lt>kevin@kesteb.usE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Kevin L. Esteb
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
--- trunk/t/boilerplate.t
+++ trunk/t/boilerplate.t
@@ -3,7 +3,7 @@
 use 5.006;
 use strict;
 use warnings;
-use Test::More tests => 5;
+use Test::More tests => 6;
 
 sub not_in_file_ok {
     my ($filename, %regex) = @_;
@@ -49,6 +49,7 @@ TODO: {
     "placeholder date/time"       => qr(Date/time)
   );
 
+  module_boilerplate_ok('lib/XAS/Apps/Database/Schema.pm');
   module_boilerplate_ok('lib/XAS/Model/Database.pm');
   module_boilerplate_ok('lib/XAS/Model/DBM.pm');
   module_boilerplate_ok('lib/XAS/Model.pm');
